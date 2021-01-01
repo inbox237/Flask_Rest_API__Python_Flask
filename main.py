34e311b046ae2dd2b1ade5c6589e73f837c9c66c
inbox237 <inbox237@yandex.com>
2021-01-01 16:18:43 +1000

    broke previous code, started with fresh

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+app.config.from_object("default_settings.app_config")
+
+from database import init_db
+db = init_db(app)
+
+from flask_marshmallow import Marshmallow
+ma = Marshmallow(app)
+
+from commands import db_commands
+app.register_blueprint(db_commands)
+
+from controllers import registerable_controllers
+
+for controller in registerable_controllers:
+    app.register_blueprint(controller)
